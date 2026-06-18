9a2d45c051166b7310bcdc04ca19551dec255c12
hispafox <hispafox@gmail.com>
2026-06-18 12:15:59 +0200

    docs: a\u00f1adir cabecera de autor, fecha y versi\u00f3n \u2014 Repositories
    Cubre issue #19. Archivos modificados:
    - Repositories/TareaRepository.cs
    - Repositories/ITareaRepository.cs
    - Repositories/CategoriaRepository.cs
    - Repositories/ICategoriaRepository.cs
    - Repositories/PersonaRepository.cs
    - Repositories/IPersonaRepository.cs

--- DemoTareas/Repositories/ITareaRepository.cs
+++ DemoTareas/Repositories/ITareaRepository.cs
@@ -1,3 +1,7 @@
+// Autor:   DemoTareas Team
+// Fecha:   2026-06-18
+// Versión: 1.0
+
 using DemoTareas.Models;
 
 namespace DemoTareas.Repositories;
@@ -5,6 +9,7 @@ namespace DemoTareas.Repositories;
 public interface ITareaRepository
 {
     IList<Tarea> GetAll();
+    IList<Tarea> GetFiltered(EstadoTarea? estado, int? categoriaId, Prioridad? prioridad);
     Tarea? GetById(int id);
     void Add(Tarea tarea);
     void Update(Tarea tarea);
